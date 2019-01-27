12833572ae172a0912d3cca790295bd28a923134
Xiaobai0419 <minexiaobai@hotmail.com>
2019-01-27 16:53:17 +0800

    Initial commit

---
+++ src/main/java/com/xiaobai/fenghuangeureka/exception/HttpStatus401Exception.java
@@ -0,0 +1,11 @@
+package com.xiaobai.fenghuangeureka.exception;
+
+public class HttpStatus401Exception extends Exception{
+    public HttpStatus401Exception(String message) {
+        super(message);
+    }
+
+    public HttpStatus401Exception() {
+        super();
+    }
+}
